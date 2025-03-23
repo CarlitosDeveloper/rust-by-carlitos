ebe8bcdcc9ebd43fb5365180f9728ac54104af65
CarlitosDeveloper <LearnWithCarlitos@gmail.com>
2025-03-22 19:28:24 -0500

    20250322-avance1

---
+++ src/bin/01-intro.rs
@@ -0,0 +1,25 @@
+fn main() {
+    println!("Hello, world! intro.rs");
+}
+
+// src\01-introduction
+
+// cargo --01-introduction
+
+// cargo run --01-introduction intro
+
+// cargo run --bin intro
+// cargo run --bin basics
+
+
+/*
+
+cargo run --bin intro
+cargo run --bin language_basics
+
+
+cargo run --bin 01-intro
+cargo run --bin language_basics
+
+
+*/
